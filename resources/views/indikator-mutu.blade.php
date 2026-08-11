6b3ff733f3f678098ce4a44f3cd7bc8dd5c0c544
Rais Atma R <raisatma79@gmail.com>
2026-08-11 08:07:04 +0700

    indikator mutu page update

--- resources/views/indikator-mutu.blade.php
+++ resources/views/indikator-mutu.blade.php
@@ -31,7 +31,7 @@
                     <!-- Bootstrap Nav Pills -->
                     <div class="nav flex-column nav-pills me-3 space-y-2" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                         @foreach($tabel_indikator_mutu as $tahun => $items)
-                            <button class="btn btn-outline-info text-start fw-semibold fs-5 text-dark hover-primary my-1 {{ $loop->first ? 'active' : '' }}" id="v-pills-{{ $tahun }}-tab" data-toggle="pill" data-target="#v-pills-{{ $tahun }}" type="button" role="tab" aria-controls="v-pills-{{ $tahun }}" aria-selected="{{ $loop->first ? 'true' : 'false' }}">
+                            <button class="btn btn-outline-info text-start fw-semibold fs-5 text-dark hover-primary my-1 {{ $loop->first ? 'active' : '' }}" id="v-pills-{{ $tahun }}-tab" data-bs-toggle="pill" data-bs-target="#v-pills-{{ $tahun }}" type="button" role="tab" aria-controls="v-pills-{{ $tahun }}" aria-selected="{{ $loop->first ? 'true' : 'false' }}">
                                 Indikator Mutu Tahun {{ $tahun }}
                             </button>
                         @endforeach
